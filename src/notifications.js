45985e90a9e629aeb6ca589f2bb70ab531e7eb16
christophior <chrisvillarreal1018@gmail.com>
2017-09-01 02:57:41 -0500

    added dismissal, animations

--- src/notifications.js
+++ src/notifications.js
@@ -1,49 +1,96 @@
 import React, { Component } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import Popover from 'react-popover'
-
-const renderNotification = (e, i) => {
-	// just changing color for different messages
-	var headerClass = ['four', 'five'].includes(e) ? 'notification_header notification_header_smc' : 'notification_header';
-	return (
-		<li key={i} className="notification">
-			<div className={headerClass}>
-				Notification {e}
-				<span className="vx_icon vx_icon-small vx_icon-close-small notification-dismiss"></span>
-			</div>
-			<div className="notification_description">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-			</div>
-			<a href="#" className="notification_link">click here</a>
-		</li>
-	);
-};
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Notifications extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			open: true
+			open: true,
+			notifications: [
+				{
+					id: 1,
+					hidden: false,
+					header: "Notification 1",
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ut labore et dolore magna aliqua."
+				},
+				{
+					id: 2,
+					hidden: false,
+					header: "Notification 2",
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ut labore et dolore magna aliqua."
+				},
+				{
+					id: 3,
+					hidden: false,
+					header: "Notification 3",
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ut labore et dolore magna aliqua."
+				}
+			],
+			messages: [
+				{
+					id: 4,
+					hidden: false,
+					header: "Notification 4",
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ut labore et dolore magna aliqua."
+				},
+				{
+					id: 5,
+					hidden: false,
+					header: "Notification 5",
+					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ut labore et dolore magna aliqua."
+				}
+			]
 		};
 		this.handleClick = this.handleClick.bind(this);
+		this.dismissNotification = this.dismissNotification.bind(this);
 		this.closePopover = this.closePopover.bind(this);
+		this.renderNotification = this.renderNotification.bind(this);
 	}
 	handleClick (e) {
 		this.setState({
 			open: !this.state.open
 		})
 	}
+	dismissNotification(id) {
+		this.setState((prevState) => {
+			console.log(prevState);
+			for (var i = 0; i<prevState.notifications.length; i++) {
+				if (prevState.notifications[i].id === id) {
+					prevState.notifications[i].hidden = true;
+					return prevState;
+				}
+			}
+			return prevState;
+		});
+	}
 	closePopover (e) {
 		this.setState({
 			open: false
 		})
+	}
+	renderNotification({id, hidden, header, description}) {
+		var headerClass = 'notification_header';
+		return (
+			hidden ? null :
+			<li key={id} className="notification">
+				<div className={headerClass}>
+					{header}
+					<span className="vx_icon vx_icon-small vx_icon-close-small notification-dismiss" onClick={() => this.dismissNotification(id)}></span>
+				</div>
+				<div className="notification_description">
+					{description}
+				</div>
+				<a href="#" className="notification_link">click here</a>
+			</li>
+		);
 	}
 	render() {
 		return (
 			<Popover isOpen={this.state.open}
 				onOuterAction={this.closePopover}
 				preferPlace={'below'}
-				enterExitTransitionDurationMs={false}
 				tipSize={10}
 				body={
 					<Tabs selectedTabClassName='selected-tab' selectedTabPanelClassName='selected-panel'>
@@ -54,12 +101,26 @@ class Notifications extends Component {
 
 						<TabPanel className='panel'>
 							<ul>
-								{ ['one', 'two', 'three'].map(renderNotification) }
+								<ReactCSSTransitionGroup
+									transitionName="notification"
+									transitionAppear={false}
+									transitionEnter={false}
+									transitionLeave={true}
+									transitionLeaveTimeout={300}>
+									{ this.state.notifications.map(this.renderNotification) }
+								</ReactCSSTransitionGroup>
 							</ul>
 						</TabPanel>
 						<TabPanel className='panel'>
 							<ul>
-								{['four', 'five'].map(renderNotification)}
+								<ReactCSSTransitionGroup
+									transitionName="notification"
+									transitionAppear={false}
+									transitionEnter={false}
+									transitionLeave={true}
+									transitionLeaveTimeout={300}>
+									{ this.state.messages.map(this.renderNotification) }
+								</ReactCSSTransitionGroup>
 							</ul>
 						</TabPanel>
 					</Tabs>
